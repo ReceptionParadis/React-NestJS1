437bfd029e3037091259ae174c9f8cd2f5d1d412
ReceptionParadis <thomas.petrissans@hotelparadislourdes.com>
2026-08-04 23:38:19 +0200

    feat(api): enable groups module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { GroupsModule } from './groups/groups.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { ShiftLogModule } from './shift-log/shift-log.module';
 import { TicketsModule } from './tickets/tickets.module';
 
 @Module({
-  imports: [ShiftLogModule, RoomsModule, TicketsModule],
+  imports: [ShiftLogModule, RoomsModule, TicketsModule, GroupsModule],
   controllers: [AppController],
 })
 export class AppModule {}
