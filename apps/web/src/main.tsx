99aba61a393f09a106c3fe2ea51a5a7a196992b5
ReceptionParadis <thomas.petrissans@hotelparadislourdes.com>
2026-08-04 21:57:09 +0200

    feat: expose shift log route

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { MainCourante } from './MainCourante';
 import './styles.css';
 
+const page = window.location.pathname === '/main-courante' ? <MainCourante /> : <App />;
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode>{page}</React.StrictMode>,
 );
