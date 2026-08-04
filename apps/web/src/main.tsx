aadc31e6cd1684122d0b39714e6ce240372e1656
ReceptionParadis <thomas.petrissans@hotelparadislourdes.com>
2026-08-04 23:35:08 +0200

    style: load groups styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -5,6 +5,7 @@ import './styles.css';
 import './shift-log.css';
 import './rooms.css';
 import './tickets.css';
+import './groups.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><AppRouter /></React.StrictMode>,
