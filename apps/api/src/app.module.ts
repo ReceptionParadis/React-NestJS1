58c629855715137fc7cf74c838ef026728fe7fa1
ReceptionParadis <thomas.petrissans@hotelparadislourdes.com>
2026-08-04 23:22:31 +0200

    feat: register rooms module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { RoomsModule } from './rooms/rooms.module';
 import { ShiftLogModule } from './shift-log/shift-log.module';
 
 @Module({
-  imports: [ShiftLogModule],
+  imports: [ShiftLogModule, RoomsModule],
   controllers: [AppController],
 })
 export class AppModule {}
