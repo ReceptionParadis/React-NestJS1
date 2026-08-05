7c4848dec7f26ea47bb7f32e1402f8b26d0b587b
ReceptionParadis <thomas.petrissans@hotelparadislourdes.com>
2026-08-05 16:21:33 +0200

    Load meeting rooms agenda styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,12 +4,11 @@ import { AppRouter } from './AppRouter';
 import './styles.css';
 import './executive-dashboard.css';
 import './shift-log.css';
-import './rooms.css';
 import './tickets.css';
 import './groups.css';
 import './rooming-import.css';
-import './allocation-review.css';
 import './auth.css';
+import './meeting-rooms.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><AppRouter /></React.StrictMode>,
