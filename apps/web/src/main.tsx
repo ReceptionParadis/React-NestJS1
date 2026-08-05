64932fa91d80fb3ac5096d65897558aa94fc756d
ReceptionParadis <thomas.petrissans@hotelparadislourdes.com>
2026-08-05 19:56:59 +0200

    style(web): load interservice control styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -12,6 +12,7 @@ import './meeting-rooms.css';
 import './restaurant-planning.css';
 import './interservice.css';
 import './weekly-planning.css';
+import './interservice-control.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><AppRouter /></React.StrictMode>,
