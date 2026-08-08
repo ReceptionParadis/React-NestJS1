7b001ec77890bc53369ef77c6b19c8b2f54f7341
ReceptionParadis <thomas.petrissans@hotelparadislourdes.com>
2026-08-08 14:14:58 +0200

    fix(build): remove missing reception archives stylesheet import

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -31,7 +31,6 @@ import './reception-control-uniform.css';
 import './group-control-print-fix.css';
 import './operational-group-buckets.css';
 import './reception-hub.css';
-import './reception-archives.css';
 import './reception-nav.css';
 import './reception-night.css';
 
