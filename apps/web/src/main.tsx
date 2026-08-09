fdd8591ecb4b15ae08e674ab40891ac1c283e9e0
ReceptionParadis <thomas.petrissans@hotelparadislourdes.com>
2026-08-09 20:22:36 +0200

    fix(function-sheet): remove validation and diffusion workflow

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -12,8 +12,6 @@ import { CommandCenterButton } from './CommandCenterButton';
 import { NightAuditorNav } from './NightAuditorNav';
 import { FunctionSheetPrintCleanup } from './FunctionSheetPrintCleanup';
 import { FunctionSheetSourceSync } from './FunctionSheetSourceSync';
-import { FunctionSheetRedistributionControl } from './FunctionSheetRedistributionControl';
-import { FunctionSheetBulkValidation } from './FunctionSheetBulkValidation';
 import { MealOrderAutoSync } from './MealOrderAutoSync';
 import { PdjBoxMealRow } from './PdjBoxMealRow';
 import { MealTransmissionDashboard } from './MealTransmissionDashboard';
@@ -32,7 +30,7 @@ import './rooming-import.css';
 import './auth.css';
 import './meeting-rooms.css';
 import './weekly-planning.css';
-import './function-sheet-validation.css';
+import './function-sheet-auto-print.css';
 import './operations-center.css';
 import './general-instructions.css';
 import './activity-journal.css';
@@ -74,8 +72,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <AppRouter />
     <FunctionSheetSourceSync />
     <FunctionSheetPrintCleanup />
-    <FunctionSheetRedistributionControl />
-    <FunctionSheetBulkValidation />
     <MealOrderAutoSync />
     <PdjBoxMealRow />
     <MealTransmissionDashboard />
